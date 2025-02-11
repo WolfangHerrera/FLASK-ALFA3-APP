a9f3e40a94249bf5cd25f359068130afc7dc7a31
wolfangherrera <andreyherrerac@icloud.com>
2025-02-10 22:59:11 -0500

    Add a hello world route to the Flask application

--- server.py
+++ server.py
@@ -10,5 +10,10 @@ APP.register_blueprint(ITEM)
 
 CORS(APP)
 
+@APP.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
 if __name__ == "__main__":
     APP.run(host='0.0.0.0', port=8081, debug=True)
