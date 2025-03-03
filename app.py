3a873859f87df09a92d6c187a7cc50e78ad82414
wolfangherrera <andreyherrerac@icloud.com>
2025-03-02 21:13:43 -0500

    Remove logging and simplify CORS handling in app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from core.controller.item.item import ITEM
 from core.controller.order.order import ORDER
 from core.controller.user.user import USER
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 app.register_blueprint(USER)
@@ -15,16 +13,6 @@ app.register_blueprint(ITEM)
 app.register_blueprint(ORDER)
 
 CORS(app, origins=["https://www.alfa3electricos.com", "https://alfa3electricos.com", "https://mercadopago.com.ar"])
-
-@app.after_request
-def after_request(response):
-    logger.info(f"RESPONSE AFTER REQUEST *****: {response}")
-    origin = request.headers.get('Origin')
-    if origin in ["https://www.alfa3electricos.com", "https://alfa3electricos.com", "https://mercadopago.com.ar"]:
-        response.headers.add('Access-Control-Allow-Origin', origin)
-    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    return response
 
 @app.route('/')
 def hello_world():
