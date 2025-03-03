c77f45c8e31bd8d384daf15f7d8edd8d5d344c41
wolfangherrera <andreyherrerac@icloud.com>
2025-03-02 20:59:04 -0500

    Add logging and enhance CORS handling for additional origins

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+import logging
+from flask import Flask, request
 from flask_cors import CORS
-
 
 from core.controller.item.item import ITEM
 from core.controller.order.order import ORDER
 from core.controller.user.user import USER
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 app.register_blueprint(USER)
@@ -15,7 +18,10 @@ CORS(app, origins=["https://www.alfa3electricos.com", "https://alfa3electricos.c
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', 'https://www.alfa3electricos.com')
+    logger.info(f"RESPONSE AFTER REQUEST *****: {response}")
+    origin = request.headers.get('Origin')
+    if origin in ["https://www.alfa3electricos.com", "https://alfa3electricos.com", "https://mercadopago.com.ar"]:
+        response.headers.add('Access-Control-Allow-Origin', origin)
     response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     return response
@@ -23,7 +29,6 @@ def after_request(response):
 @app.route('/')
 def hello_world():
     return 'Hello World! from WOLFANGDEVS.COM'
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8081, debug=True)
