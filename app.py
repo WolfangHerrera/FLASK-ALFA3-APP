995bf7a8ef35fbebd19c4fed906373e8e295e6a4
wolfangherrera <andreyherrerac@icloud.com>
2025-05-02 21:49:14 -0500

    Refactor app structure to include CART blueprint and clean up ORDER controller

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import logging
-from flask import Flask, request
+from flask import Flask
 from flask_cors import CORS
 
 from core.controller.item.item import ITEM
+from core.controller.cart.cart import CART
 from core.controller.order.order import ORDER
 from core.controller.user.user import USER
 
@@ -10,7 +10,9 @@ from core.controller.user.user import USER
 app = Flask(__name__)
 app.register_blueprint(USER)
 app.register_blueprint(ITEM)
+app.register_blueprint(CART)
 app.register_blueprint(ORDER)
+
 
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 # CORS(app, origins=["https://alfa3.wolfangdevs.com", "https://alfa3electricos.com", "https://mercadopago.com"])
